723924d4a9435033150d389e7f4541c53463391b
kirstyabhus <kabhulimhen@gmail.com>
2022-11-03 16:04:19 +0000

    objects of Product class

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    Product[] products = new Product[]{
+            new Product("water", 1.0F),
+            new Product("sprite", 1.2F),
+            new Product("cola", 1.3F),
+            new Product("fanta", 1.1F),
+            new Product("orange", 1.5F)
+    };
+    public static void main(String[] args) {
+
+        System.out.println("+-------------------------+");
+        System.out.println("|     Vending Machine     |");
+        System.out.println("+-------------------------+");
+    }
+}
